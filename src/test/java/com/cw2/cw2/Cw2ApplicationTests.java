5de35e44a77183a9ec522bfc1f0ef3f13b458924
maks <maxim_86nva@mail.ru>
2023-11-11 11:58:34 +0300

    init project

---
+++ src/test/java/com/cw2/cw2/Cw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cw2.cw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
